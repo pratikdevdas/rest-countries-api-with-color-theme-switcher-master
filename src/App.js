0d2de543e4a27895ecc24c5d8b0d152eedc4fd63
Revenant <dasdev.pratik@gmail.com>
2022-03-28 12:56:36 +0530

    Refactored regions 👌

--- src/App.js
+++ src/App.js
@@ -1,66 +1,41 @@
-import "./App.css";
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import Region from "./Region";
+import './App.css'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Region from './Region'
+import { Navbar } from './Navbar'
 
 function App() {
-  const [countries, setcountries] = useState([]);
-  const [search, setsearch] = useState("");
-  const [regionname, setRegionName] = useState('')
-  const [regionFilter, setRegionFilter] = useState("")
+
+  const [countries, setcountries] = useState([])
+  const [search, setsearch] = useState('')
 
   useEffect(() => {
     axios
-      .get("https://restcountries.com/v3.1/all")
-      .then((response) => setcountries(response.data));
-  }, []);
+      .get('https://restcountries.com/v3.1/all')
+      .then((response) => setcountries(response.data))
+  }, [])
 
   const handleChange = (e) => {
-    e.preventDefault();
-    setsearch(e.target.value);
-  };
-  
+    e.preventDefault()
+    setsearch(e.target.value)
+  }
+
   const filter = countries.filter(c =>
-  c.name.common.toLowerCase().includes(search.toLowerCase())
-);
+    c.name.common.toLowerCase().includes(search.toLowerCase())
+  )
 
-const regionSearch = (e)=>{
-  e.preventDefault()
-  setRegionFilter(regionname)
-}
-
-const region = filter.filter(c => c.region.toLowerCase().includes(regionFilter.toLowerCase()) )
-console.log(region)
-  const randomstyles = { maxWidth: '100%', maxHeight:'50%' }
 
   return (
     <div>
-      search region
-      <form onSubmit={regionSearch}>
-      <input value={regionname} onChange={(event)=>setRegionName(event.target.value)}/>
-      <button onClick={regionSearch}>search by region</button>
-      </form>
+      <Navbar/>
       <div>
       search countries
-      <input type="text" onChange={handleChange} />
+        <input type="text" onChange={handleChange} />
       </div>
-      <Region/>
-      <div className="container">
-        {region.map((c) => (
-          <div className="box" key={c.id}>
-            <div>
-              <img src={c.flags.png} alt="" style={randomstyles} />
-              </div>
-            <div>{c.name.common}</div>
-            <div>{c.population}</div>
-            <div>{c.region}</div>
-            <div>{c.capital}</div>
-          </div>
-        ))}
-      </div>
-
+      <div>Region</div>
+      <Region filter={filter}/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/Region.js
+++ src/Region.js
@@ -1,37 +1,36 @@
-import { useState } from "react";
+import { useState } from 'react'
+import { ShowRegions } from './Toggle'
 
+const Region = ({ filter }) => {
+  const [regionName, setRegionName] = useState('')
+  const region = filter.filter(c => c.region.toLowerCase().includes(regionName.toLowerCase()) )
 
-const Region = () => {
-
-    const [visible, setvisible] = useState(false)
-
-
-    const showRegions = () => {
-        // this will be hidden when login form is visible
-        const hideWhenVisible = {display: visible ? 'none':''} 
-        // this will be shown when login form is visible
-        const showWhenVisible = {display: visible ? '':'none'}
-        
-        return (
-      
+  const randomstyles = { maxWidth: '100%', maxHeight:'50%' }
+  return(
+    <>
+      <ShowRegions buttonLabel='show' buttonLabel2='hide'>
+        <div>
+          <button onClick={() => setRegionName('america')}>AMERICA</button>
+          <button onClick={() => setRegionName('africa')}>AFRICA</button>
+          <button onClick={() => setRegionName('asia')}>ASIA</button>
+          <button onClick={() => setRegionName('europe')}>EUROPE</button>
+          <button onClick={() => setRegionName('oceania')}>OCEANIA </button>
+        </div>
+      </ShowRegions>
+      <div className="container">
+        {region.map((c) => (
+          <div className="box" key={c.id}>
             <div>
-    <div>Region</div>
-    <div style={hideWhenVisible}>
-        <button onClick={()=> setvisible(true)}>show</button>
-    </div>
-    <div style={showWhenVisible}>
-        <button onClick={()=> setvisible(false)}>hide</button>
-        <li>dks</li>
-        <li>SDF</li>
-        <li>FDS</li>
-        <li>FSD</li>
-    </div>
-    </div>
+              <img src={c.flags.png} alt="" style={randomstyles} />
+            </div>
+            <div>{c.name.common}</div>
+            <div>{c.population}</div>
+            <div>{c.region}</div>
+            <div>{c.capital}</div>
+          </div>
+        ))}
+      </div></>
   )
-}
-return(
-    <>{showRegions()}</>
-)
 }
 
 export default Region
