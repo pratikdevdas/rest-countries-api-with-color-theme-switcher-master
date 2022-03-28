b4450505d3a12eedef60003ff04826a629991340
Revenant <dasdev.pratik@gmail.com>
2022-03-28 15:37:18 +0530

    datainsetion form json

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,13 +1,33 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams,useNavigate } from 'react-router-dom'
 
 const Country = ({ countries }) => {
   const name = useParams().id
   const singleCountryArray = countries.find(n => n.name.common === name)
+  const navigate = useNavigate()
+
+  const backButton = (event) => {
+    event.preventDefault()
+    navigate('/')
+  }
 
   console.log(singleCountryArray.name.common)
   return (
-    <div>{singleCountryArray.name.common}</div>
+    <div>
+      <button onClick={backButton}>back button</button>
+
+      <img src={singleCountryArray.flags.png} alt="" />
+      {singleCountryArray.name.common}
+      {/* native name {singleCountryArray.nativeName.spa.common} */}
+        population{singleCountryArray.population}
+        region{singleCountryArray.region}
+        subregion{singleCountryArray.subregion}
+        capital{singleCountryArray.capital}
+        topleveldomain{singleCountryArray.tld[0]}
+      {/* currency{singleCountryArray.currencies[0].name} */}
+      {/* languages{singleCountryArray.laguages.spa} */}
+      <div>botder countries{singleCountryArray.borders}</div>
+    </div>
   )
 }
 
